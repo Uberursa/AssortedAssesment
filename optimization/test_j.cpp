1566ed1af73cf7639158c71b292b33e283ca6a58
Holly <uberursa@gmail.com>
2023-12-09 16:05:59 -0500

    Started the process of removing the repeated work

--- optimization/test_j.cpp
+++ optimization/test_j.cpp
@@ -4,53 +4,112 @@
 
 // gcc -lm test_j.c -o test_j
 
-double function_j(double f, double fp, double fptilde) {
- 
-   double a  = 0.0081;
-   double b  = 0.6;
-   double g  = 9.807;
-   double pi = 4.*atan(1.);
+struct Vars_j
+{
+    double a;
+   double b;
+   double g;
+   double pi;
 
-   double fptildemin = (1.0/2.0/pi) * pow((4.0 * b / 5.0), (1.0/4.0));
+   double fptildemin;
 
-   double gC = 5.87;
-   double aC = 0.0317;
+   double gC;
+   double aC;
 
-   double aX  = (log(a)-log(aC))/log(fptildemin);
-   double gX  = -log(gC)/log(fptildemin);
+   double aX;
+   double gX;
 
-   double saC = 0.0547;
-   double saX = 0.32;
+   double saC;
+   double saX;
 
-   double sbC = 0.0783;
-   double sbX = 0.16;
+   double sbC;
+   double sbX;
 
-   double fpt = MAX(fptilde, fptildemin);
+   double fpt;
 
-   double alpha   = aC  * pow(fpt, aX);
-   double gamma   = gC  * pow(fpt, gX);
-   double sigma_a = saC * pow(fpt, saX);
-   double sigma_b = sbC * pow(fpt, sbX);
-   double exp1arg = -1.25 * pow((f/fp),-4);
-   double sigma   = (f <= fp) * sigma_a + (f > fp) * sigma_b;
+   double alpha;
+   double gamma;
+   double sigma_a;
+   double sigma_b;
+   double exp1arg;
+   double sigma;
 
-   double exp2arg = -0.5 * pow((f-fp)/(sigma*fp), 2);
+   double exp2arg;
 
-   double S = alpha * pow(g, 2) * pow((2*pi), -4) * pow(f,-5) * exp(exp1arg) * pow(gamma, exp(exp2arg));
+   double S;
+};
 
-   return S;
+
+double function_j(double f, double fp, double fptilde, Vars_j *vars) {
+
+   vars->exp1arg = -1.25 * pow((f/fp),-4);
+   vars->sigma   = (f <= fp) * vars->sigma_a + (f > fp) * vars->sigma_b;
+
+   vars->exp2arg = -0.5 * pow((f-fp)/(vars->sigma*fp), 2);
+
+   vars->S = vars->alpha * pow(vars->g, 2) * pow((2*vars->pi), -4) * pow(f,-5) * exp(vars->exp1arg) * pow(vars->gamma, exp(vars->exp2arg));
+
+   return vars->S;
+}
+
+void setConstants_j(Vars_j *vars) {
+    vars->a  = 0.0081;
+    vars->b  = 0.6;
+    vars->g  = 9.807;
+    vars->pi = 4.*atan(1.);
+
+    vars->fptildemin = (1.0/2.0/vars->pi) * pow((4.0 * vars->b / 5.0), (1.0/4.0));
+
+    vars->gC = 5.87;
+    vars->aC = 0.0317;
+
+    vars->aX  = (log(vars->a)-log(vars->aC))/log(vars->fptildemin);
+    vars->gX  = -log(vars->gC)/log(vars->fptildemin);
+
+    vars->saC = 0.0547;
+    vars->saX = 0.32;
+
+    vars->sbC = 0.0783;
+    vars->sbX = 0.16;
 }
 
 int main() {
 
   double S, f, fp, fptilde;
+  Vars_j vars;
 
-  for (f = -5.; f <= 5.; f += 0.01) {
-    for (fp = 0.; fp <= 10.; fp += 0.01) {
-      for (fptilde = 0.; fptilde <= 10.; fptilde += 0.01) {
-        S = function_j(f, fp, fptilde);
-      }
+//   for (f = -5.; f <= 5.; f += 0.01) {
+//     for (fp = 0.; fp <= 10.; fp += 0.01) {
+//       for (fptilde = 0.; fptilde <= 10.; fptilde += 0.01) {
+//         S = function_j(f, fp, fptilde);
+//       }
+//     }
+//   }
+
+//Stage 1, constants
+setConstants_j(&vars);
+
+// stripped down version of the loop for faster testing
+//   for (f = -5.; f <= 5.; f += 0.1) {
+//     for (fp = 0.; fp <= 10.; fp += 0.1) {
+//       for (fptilde = 0.; fptilde <= 10.; fptilde += 0.1) {
+//         S = function_j(f, fp, fptilde);
+//       }
+//     }
+//   }
+
+    for (fptilde = 0.; fptilde <= 10.; fptilde += 0.1) {
+        vars.fpt = MAX(fptilde, vars.fptildemin);
+
+        vars.alpha   = vars.aC  * pow(vars.fpt, vars.aX);
+        vars.gamma   = vars.gC  * pow(vars.fpt, vars.gX);
+        vars.sigma_a = vars.saC * pow(vars.fpt, vars.saX);
+        vars.sigma_b = vars.sbC * pow(vars.fpt, vars.sbX);
+
+        for (f = -5.; f <= 5.; f += 0.1) {
+            for (fp = 0.; fp <= 10.; fp += 0.1) {
+                S = function_j(f, fp, fptilde, &vars);
+            }
+        }
     }
-  }
-
 }
